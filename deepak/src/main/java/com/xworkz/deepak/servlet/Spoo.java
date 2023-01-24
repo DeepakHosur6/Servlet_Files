3881db9581cd5d72a9d7ab26e374d1a6a783e828
deepakhosur6 <deepakh.xworkz@gmail.com>
2023-01-24 23:18:39 +0530

    Servlet Day 2 Task

---
+++ deepak/src/main/java/com/xworkz/deepak/servlet/Spoo.java
@@ -0,0 +1,25 @@
+package com.xworkz.deepak.servlet;
+//23-01-2023 Day2 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(loadOnStartup = 2, urlPatterns = "/yaru")
+public class Spoo extends HttpServlet {
+
+	public Spoo() {
+		// TODO Auto-generated constructor stub
+
+		System.out.println("Constructor created :"+this.getClass().getSimpleName());
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("Running doGet method inside spoo");
+
+	}
+}
